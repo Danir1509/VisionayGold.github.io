4c30d5cebab104b824eb0f21f3c0a6ce29a14f2b
Danir1509 <2524572019@mail.utec.edu.sv>
2024-10-24 18:39:43 -0600

    actu

---
+++ wwwroot/js/Card.js
@@ -0,0 +1,16 @@
+﻿const cards = document.querySelectorAll('.card');
+
+// Manejo del movimiento del mouse para rotar las tarjetas
+document.addEventListener('mousemove', (event) => {
+    const { clientX, clientY } = event;
+    const { innerWidth, innerHeight } = window;
+
+    // rotación basada en la posición del mouse
+    const rotateY = ((clientX / innerWidth) - 0.5) * 30; // Ajusta la sensibilidad
+    const rotateX = ((clientY / innerHeight) - 0.5) * -30; // Ajusta la sensibilidad
+
+    // transformaciones a todas las tarjetas
+    cards.forEach(card => {
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+});
